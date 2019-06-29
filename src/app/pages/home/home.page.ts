26a4112c67a5733195c3fc3b360cd701d631b1be
issam <issam71100@gmail.com>
2019-06-29 20:02:40 +0000

    add style index hike

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ItemService } from '../../services/item.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ItemService} from '../../services/item.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  items: Array<any>;
+    items: Array<any>;
 
-  constructor(
-    private router: Router,
-    public itemService: ItemService
-  ){}
+    constructor(
+        private router: Router,
+        public itemService: ItemService
+    ) {
+    }
 
-  ngOnInit(){
-    this.items = this.itemService.getItems();
-  }
+    ngOnInit() {
+        this.items = this.itemService.getItems();
+    }
 
 }
